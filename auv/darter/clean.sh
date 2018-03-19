e789ba011ee10db928e4194f569fdd795ff196e7
Erin Fischell <efischell@whoi.edu>
2018-03-19 17:09:08 -0400

    working version with loiter and racetrack

---
+++ auv/darter/clean.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+rm -rf   MOOSLog_*
+rm -f    *~
+rm -f    *.moos++
+rm -f    .LastOpenedMOOSLogDirectory
+rm -f last.moos
+rm -f current.bhv
+rm -f allconfig.ydef
+rm -f allconfig.yaml
